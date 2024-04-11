95e9fe5c699db263931290947d1e685a00057e0a
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-04-11 14:46:15 +0300

    selector section new

---
+++ src/js/inputs.js
@@ -0,0 +1,11 @@
+const optionsMenu1 = document.getElementById('options1');
+
+document.getElementById('trigger1').addEventListener('click', () => {
+  optionsMenu1.style.display = 'block';
+});
+
+const optionsMenu2 = document.getElementById('options2');
+
+document.getElementById('trigger2').addEventListener('click', () => {
+  optionsMenu2.style.display = 'block';
+});
