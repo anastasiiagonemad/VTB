3bea5917624cd750833a0b08646ac1b2ab92b186
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-04-11 09:58:53 +0300

    fix slider

--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
 import './style/main.css';
 import './style/normalize.css';
 import './fonts/fonts.scss';
-
-document.addEventListener('DOMContentLoaded', function () {
-  var slides = document.querySelectorAll('.slide');
-  var dots = document.querySelectorAll('.dot');
-  var currentSlide = 0;
-
-  // Добавляем обработчики событий для кнопок навигации
-  dots.forEach(function (dot, index) {
-    dot.addEventListener('click', function () {
-      showSlide(index);
-    });
-  });
-
-  function showSlide(n) {
-    // Скрываем все слайды
-    slides.forEach(function (slide) {
-      slide.classList.remove('active');
-    });
-
-    // Показываем выбранный слайд и обновляем активную кнопку
-    slides[n].classList.add('active');
-    currentSlide = n;
-  }
-
-  // Показываем первый слайд при загрузке страницы
-  showSlide(currentSlide);
-});
+import './js/slider.js';
---
+++ src/js/slider.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.querySelectorAll('.slide');
+  const dots = document.querySelectorAll('.dot');
+  let currentSlide = 0;
+
+  dots.forEach(function (dot, index) {
+    dot.addEventListener('click', function () {
+      showSlide(index);
+    });
+  });
+
+  function showSlide(n) {
+    slides.forEach(function (slide) {
+      slide.classList.remove('active');
+    });
+
+    slides[n].classList.add('active');
+    currentSlide = n;
+  }
+
+  showSlide(currentSlide);
+});
