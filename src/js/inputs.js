80ea52d921309e6694a79f42f320592f2170ca98
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-04-12 00:35:33 +0300

    fix for custom select

--- src/js/inputs.js
+++ src/js/inputs.js
@@ -1,11 +1,25 @@
-const optionsMenu1 = document.getElementById('options1');
+//FUNCTION SELECT_DROPDOWN
+function showDropdown(id) {
+  const dropdown = document.getElementById(id);
+  if (dropdown.style.display === 'none') {
+    dropdown.style.display = 'block';
+  } else {
+    dropdown.style.display = 'none';
+  }
+}
+//ACTION DROPDOWN
+document
+  .getElementById('dropdown1')
+  .addEventListener('click', function (event) {
+    event.stopPropagation();
+    const dropdownContent = document.getElementById('content1');
+    showDropdown('content1');
+  });
 
-document.getElementById('trigger1').addEventListener('click', () => {
-  optionsMenu1.style.display = 'block';
-});
-
-const optionsMenu2 = document.getElementById('options2');
-
-document.getElementById('trigger2').addEventListener('click', () => {
-  optionsMenu2.style.display = 'block';
-});
+document
+  .getElementById('dropdown2')
+  .addEventListener('click', function (event) {
+    event.stopPropagation();
+    const dropdownContent = document.getElementById('content2');
+    showDropdown('content2');
+  });
