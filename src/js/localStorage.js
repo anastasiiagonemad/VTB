c1b1b22cf83b8e217f4db924eb69455ccb9b2b30
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-04-16 08:09:45 +0300

    feat: add draft function create blocks

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -5,3 +5,14 @@ export function refreshLocalStorage(obj) {
   goalsArr.push(obj);
   localStorage.setItem('goalsArr', JSON.stringify(goalsArr));
 }
+
+// let goalsArr = localStorage.getItem('goalsArr') ? JSON.parse(localStorage.getItem('goalsArr')) : [];
+// function createBlocks (arr) {
+//   if (arr && arr.length !== 0) {
+//     arr.forEach(obj => () {
+//       тут какая-то вёрстка для создания блока и в неё вставляешь в нужных местах `${obj.itemGoalName}`
+//       `${obj.itemGoalSum}` и тд по списку для каждого объекта массива goalsArr, который сохранён в local storage
+//     })
+//   }
+// }
+// createBlocks(goalsArr);
