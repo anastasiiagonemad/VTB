67f836d620d8ded329c04a1d3dc8fd4385d836d8
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-04-16 09:10:33 +0300

    feat: modify function refreshLocalStorage

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,18 +1,5 @@
 'use strict'
 
 export function refreshLocalStorage(obj) {
-  let goalsArr = localStorage.getItem('goalsArr') ? JSON.parse(localStorage.getItem('goalsArr')) : [];
-  goalsArr.push(obj);
-  localStorage.setItem('goalsArr', JSON.stringify(goalsArr));
+  localStorage.setItem(obj.itemGoalName, JSON.stringify(obj));
 }
-
-// let goalsArr = localStorage.getItem('goalsArr') ? JSON.parse(localStorage.getItem('goalsArr')) : [];
-// function createBlocks (arr) {
-//   if (arr && arr.length !== 0) {
-//     arr.forEach(obj => () {
-//       тут какая-то вёрстка для создания блока и в неё вставляешь в нужных местах `${obj.itemGoalName}`
-//       `${obj.itemGoalSum}` и тд по списку для каждого объекта массива goalsArr, который сохранён в local storage
-//     })
-//   }
-// }
-// createBlocks(goalsArr);
