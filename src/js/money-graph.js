3828ec77a0a6dba968ee224d770318dedd9177d2
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-04-19 18:51:07 +0300

    add payments to money-goal graph

--- src/js/money-graph.js
+++ src/js/money-graph.js
@@ -9,35 +9,55 @@ import Chart from 'chart.js/auto';
   const date = `${day}.${month}.${year}`;
 
   if (localStorage.length) {
+    const paymentsData = [];
     for (let i = 0; i < localStorage.length; i++) {
       let key = localStorage.key(i);
       let value = localStorage.getItem(key);
 
       let obj = JSON.parse(value);
 
-      const arrPayments = obj.itemPayments;
-      arrPayments.forEach((payment) => {
-        const data = [
-          { date: date, count: payment },
-          { date: date, count: payment },
-        ];
+      const itemPayments = obj.itemPayments;
 
-        new Chart(document.getElementById('money-graph'), {
-          type: 'line',
-          data: {
-            labels: data.map((row) => row.date),
-            datasets: [
-              {
-                label: 'Сумма накопления',
-                data: data.map((row) => row.count),
-              },
-            ],
-          },
-          options: {
-            adaptive: true,
-            maintainAspectRatio: false,
-          },
+      //title money-graph
+      let totalSum = 0;
+      totalSum = itemPayments.reduce((acc, currentValue) => {
+        acc + currentValue, 0;
+      });
+      const formattedTotalSum = new Intl.NumberFormat('ru-RU').format(totalSum);
+
+      const moneyGraphTitle = document.querySelector('.start__money-sum');
+      moneyGraphTitle.innerHTML = `${formattedTotalSum} руб.`;
+
+      //take all payments from array
+      itemPayments.forEach((payment) => {
+        const date = new Date(obj.itemDateStart);
+        const formattedDate = date.toLocaleDateString('ru-RU', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit',
         });
+
+        paymentsData.push({ date: formattedDate, count: payment });
+      });
+    }
+    //add all payments to graph
+    if (paymentsData.length > 0) {
+      const ctx = document.getElementById('money-graph').getContext('2d');
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: paymentsData.map((row) => row.date),
+          datasets: [
+            {
+              label: 'Сумма накопления',
+              data: paymentsData.map((row) => row.count),
+            },
+          ],
+        },
+        options: {
+          adaptive: true,
+          maintainAspectRatio: false,
+        },
       });
     }
   }
